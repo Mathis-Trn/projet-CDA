95f83208ca93f764352826581b2d155215d0abf7
Mathis Ternon <40760027+Mathis-Trn@users.noreply.github.com>
2024-03-04 22:53:26 +0100

    Database Migrations

--- database/migrations/2024_03_04_205211_create_borrowed_items_table.php
+++ database/migrations/2024_03_04_205211_create_borrowed_items_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('borrowed_items', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('adresse')->nullable();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('borrowed_id')->constrained();
+            $table->foreignId('book_id')->constrained();
+            $table->string('status');
+            $table->timestamp('book_return')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('borrowed_items');
     }
 };
