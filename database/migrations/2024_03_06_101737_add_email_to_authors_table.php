c2788721088dfdcadf8644e4d944656f5164e4cb
Mathis Ternon <40760027+Mathis-Trn@users.noreply.github.com>
2024-03-06 22:18:29 +0100

    Modifying migrations and factories

--- database/migrations/2024_03_06_101737_add_email_to_authors_table.php
+++ database/migrations/2024_03_06_101737_add_email_to_authors_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('authors', function (Blueprint $table) {
-            //
+            $table->dropColumn("email");
         });
     }
 };
--- database/migrations/2024_03_06_101744_add_email_to_editors_table.php
+++ database/migrations/2024_03_06_101744_add_email_to_editors_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('editors', function (Blueprint $table) {
-            //
+            $table->dropColumn("email");
         });
     }
 };
