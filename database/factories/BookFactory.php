b41f7e89a1e3e5c696f0d1e90859e2e3003d0d00
Mathis Ternon <40760027+Mathis-Trn@users.noreply.github.com>
2024-03-06 11:09:49 +0100

    Create factories

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'editor_id' => fake()->numberBetween(1, 10),
+            'name' => fake()->words(2, true),
+            'description' => fake()->text(),
+            'stock' => fake()->numberBetween(1, 100),
+        ];
+    }
+}
