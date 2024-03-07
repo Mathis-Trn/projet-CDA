0b085a94d415a5592b701deff39e218b411f04df
Mathis Ternon <40760027+Mathis-Trn@users.noreply.github.com>
2024-03-07 21:45:11 +0100

    Create Vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,10 @@ import './bootstrap';
 
 import { createApp } from 'vue';
 
+import UsersList from './components/users/UsersList.vue';
+
 const app = createApp({});
 
-app.component('', '')
+app.component('usersList', UsersList)
 
 app.mount('#app');
