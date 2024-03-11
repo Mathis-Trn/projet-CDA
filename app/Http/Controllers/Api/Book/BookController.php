a553485e5e2593c9ff2c28dbd6f45f66c1cf8953
Mathis Ternon <40760027+Mathis-Trn@users.noreply.github.com>
2024-03-11 22:18:50 +0100

    Feat: Books api

---
+++ app/Http/Controllers/Api/Book/BookController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api\Book;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(Book::all(), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'editor' => 'required|string|max:255',
+            'author' => 'required|string|max:255',
+            'name' => 'required|string|max:255', 
+            'cover' => 'required|url',
+            'description' =>'required|string',
+            'published_at' =>'required|date',
+        ]);
+
+        $book = Book::create($validatedData);
+
+
+        return response()->json($book, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $book = Book::find($id);
+
+        if ($book) {
+            return response()->json($book, 200);
+        } else {
+       return response()->json(['message' => 'Livre non trouvé'], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Book $book)
+    {
+        $validatedData = $request->validate([
+            'editor' =>'required|string|max:255',
+            'author' =>'required|string|max:255',
+            'name' =>'required|string|max:255', 
+            'cover' =>'required|url',
+            'description' =>'required|string',
+            'published_at' =>'required|date',
+        ]);
+
+        if ($book) {
+            $book->update($validatedData);
+            return response()->json($book, 200);
+        } else {
+            return response()->json(['message' => 'Livre non trouvé'], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Book $book)
+    {
+        if ($book) {
+            $book->delete();
+            return response()->json(null, 204);
+        } else {
+            return response()->json(['message' => 'Livre non trouvé'], 404);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\Book\BookController;
 use App\Http\Controllers\Api\User\IndexController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('users', IndexController::class)->name('user');
+Route::apiResource('books', BookController::class);
+
+Route::get('users', IndexController::class);
