9a2623e820ead96fe1c83636f29a99f79b80d509
Mathis Ternon <40760027+Mathis-Trn@users.noreply.github.com>
2024-03-11 22:21:29 +0100

    Fix: Vue files Change of location

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,22 @@
 import './bootstrap';
 
 import { createApp } from 'vue';
-import AppNavBar from './components/AppNavBar.vue';
+import AppNavBar from './components/template/AppNavBar.vue';
 import UsersList from './components/users/UsersList.vue';
-import AppFooter from './components/AppFooter.vue';
+import AppFooter from './components/template/AppFooter.vue';
 
+const appNav = createApp({});
+const appFooter = createApp({});
 const app = createApp({});
 
-app
-    .component('app-navbar', AppNavBar)
-    .component('usersList', UsersList)
-    .component('app-footer', AppFooter);
+appNav
+  .component('app-navbar', AppNavBar)
+appNav.mount("#app-nav-bar");
 
+appFooter
+   .component('app-footer', AppFooter)
+appFooter.mount("#app-footer");
+
+app
+    .component('usersList', UsersList)
 app.mount('#app');
