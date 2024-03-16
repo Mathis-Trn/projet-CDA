bbb7540e158e71498d5be1d9008694a3960fe9fe
Mathis Ternon <40760027+Mathis-Trn@users.noreply.github.com>
2024-03-16 11:15:12 +0100

    Feat: Create Api Routes

--- routes/api.php
+++ routes/api.php
@@ -9,15 +9,29 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //--- BOOK ---//
-Route::apiResource('books', BookController::class);
+Route::get('/', [BookController::class, 'index']);
+Route::get('/book/{book}', [BookController::class, 'show']);
+Route::post('/create-book', [BookController::class,'store']);
+Route::put('/edit-book/{book}', [BookController::class, 'update']);
+Route::delete('/book/{book}', [BookController::class, 'destroy']);
 
 //--- AUTHOR ---//
-Route::apiResource('authors', AuthorController::class);
+Route::get('/authors', [AuthorController::class, 'index']);
+Route::get('/author/{author}', [AuthorController::class,'show']);
+Route::post('/create-author', [AuthorController::class,'store']);
+Route::put('/edit-author/{author}', [AuthorController::class, 'update']);
+Route::delete('/author/{author}', [AuthorController::class, 'destroy']);
 
 //--- EDITOR ---//
-Route::apiResource('editors', EditorController::class);
+Route::get('/editors', [EditorController::class, 'index']);
+Route::get('/editor/{editor}', [EditorController::class,'show']);
+Route::post('/create-editor', [EditorController::class,'store']);
+Route::put('/edit-editor/{editor}', [EditorController::class, 'update']);
+Route::delete('/editor/{editor}', [EditorController::class, 'destroy']);
 
 //--- AUTH ---//
 Route::post('/inscription', [AuthController::class, 'register']);
 Route::post('/connexion', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/deconnexion', [AuthController::class, 'logout']);
+
+Route::get('users', IndexController::class);
