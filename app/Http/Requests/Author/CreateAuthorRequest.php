79c18fd87a2a148d1427cc50eb25e6d2ca51b6b3
Mathis Ternon <40760027+Mathis-Trn@users.noreply.github.com>
2024-03-16 10:44:04 +0100

    Feat : Create Requests

---
+++ app/Http/Requests/Author/CreateAuthorRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests\Author;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class CreateAuthorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'errors' => true,
+            'message' => 'Erreur de validation',
+            'errorsList' => $validator->errors()
+        ]));
+    }
+
+    public function messages()
+    {
+
+        return [
+            'name.required' => 'Le nom est obligatoire',
+            'name.string' => 'Le nom doit être une chaîne de caractères',
+            'name.max' => 'Le nom ne doit pas dépasser 255 caractères',
+            'description.required' => 'La description est obligatoire',
+            'description.string' => 'La description doit être une chaîne de caractère',
+            'description.max' => 'La description ne doit pas dépasser 255 caractères',
+        ];
+    }
+}
