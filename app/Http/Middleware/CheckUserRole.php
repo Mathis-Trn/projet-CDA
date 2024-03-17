7f0e6ba15d6d7dea3deed0c2f6d10a073309f100
Mathis Ternon <40760027+Mathis-Trn@users.noreply.github.com>
2024-03-17 10:36:43 +0100

    Feat : Check User Role

--- app/Http/Middleware/CheckUserRole.php
+++ app/Http/Middleware/CheckUserRole.php
@@ -8,10 +8,24 @@ class CheckUserRole
 {
     public function handle($request, Closure $next)
     {
-        if ($request->user() && $request->user()->role_id != 4) {
-            return response()->json(['status_message' => 'Vous n\'avez pas la permission d\'accéder à cette page.'], 403);
+        // Vérifier si l'utilisateur est authentifié
+        if ($request->user()) {
+            // Récupérer le rôle de l'utilisateur
+            $userRole = $request->user()->role_id;
+
+            // Vérifier si l'utilisateur a le rôle 4 (peut créer, éditer, supprimer)
+            if ($userRole == 4) {
+                return $next($request);
+            }
+
+            // Si l'utilisateur n'a pas le rôle 4, vérifier s'il accède à sa propre ressource
+            $userId = $request->route('user');
+            if ($userId == $request->user()->id) {
+                return $next($request);
+            }
         }
 
-        return $next($request);
+        // Si l'utilisateur n'a pas les autorisations requises, retourner une réponse 403
+        return response()->json(['status_message' => 'Vous n\'avez pas la permission d\'accéder à cette page.'], 403);
     }
 }
